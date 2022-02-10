ee17e42038375bf441787fc65c5108cfe9b4abcd
Kitten_Bill_from_ubuntu <billlee@hhu.edu.cn>
2022-02-10 16:27:44 +0800

    commiting linked_list.c (not tested) to dev

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -8,31 +8,34 @@
 typedef uint64_t pkey_t;
 typedef uint64_t pval_t;
 
-typedef struct entry {
+typedef struct entry
+{
     pkey_t key;
     pval_t value;
-}entry_t;
+} entry_t;
 
-struct ll_node {
+struct ll_node
+{
     entry_t e;
     spinlock_t lock;
-    struct ll_node* next;
-}
+    struct ll_node *next;
+};
 
-struct linked_list {
+struct linked_list
+{
     struct ll_node *head, *tail;
-}
+};
 
-#define node_key(node) ((node)->e.k)
-#define node_val(node) ((node)->e.v)
+#define node_key(node) ((node)->e.key)
+#define node_val(node) ((node)->e.value)
 
-extern struct linked_list ll_init();
-extern int ll_insert(struct linked_list* ll, pkey_t key, pval_t val);
-extern int ll_update(struct linked_list* ll, pkey_t key, pval_t val);
-extern int ll_lookup(struct linked_list* ll, pkey_t key);
-extern int ll_remove(struct linked_list* ll, pkey_t key);
-extern int ll_range(struct linked_list* ll, pkey_t low, pkey_t high, entry_t* res_arr);
-extern void ll_print(struct linked_list* ll);
-extern void ll_destory(struct linked_list* ll);
+extern struct linked_list *ll_init();
+extern int ll_insert(struct linked_list *ll, pkey_t key, pval_t val);
+extern int ll_update(struct linked_list *ll, pkey_t key, pval_t val);
+extern int ll_lookup(struct linked_list *ll, pkey_t key);
+extern int ll_remove(struct linked_list *ll, pkey_t key);
+extern int ll_range(struct linked_list *ll, pkey_t low, pkey_t high, entry_t *res_arr);
+extern void ll_print(struct linked_list *ll);
+extern void ll_destory(struct linked_list *ll);
 
-#endif
+#endif //LINKED_LIST_H
