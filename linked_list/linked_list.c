3089e1ea8c139471df3f709f6ee805dcd5507f69
billyg <overtaker_ric@qq.com>
2022-02-10 11:47:39 +0800

    linked list

---
+++ linked_list/linked_list.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <pthread.h>
+#include <assert.h>
+#include <unistd.h>
+
+#include "linked_list.h"
+
+struct linked_list ll_init() {
+    struct linked_list* ll;
+
+    return ll;
+}
+
+int ll_insert(struct linked_list* ll, pkey_t key, pval_t val) {
+    return -EEXIST;
+    
+    return 0;
+}
+
+int ll_update(struct linked_list* ll, pkey_t key, pval_t val) {
+    return -EEXIST;
+
+    return 0;
+}
+
+int ll_lookup(struct linked_list* ll, pkey_t key) {
+    return -ENOENT;
+
+    return 0;
+}
+
+int ll_remove(struct linked_list* ll, pkey_t key) {
+    return -ENOENT;
+
+    return 0;
+}
+
+int ll_range(struct linked_list* ll, pkey_t low, pkey_t high, entry_t* res_arr) {
+    int size;
+
+    return size;
+}
+
+void ll_print(struct linked_list* ll) {
+    
+}
+
+void ll_destory(struct linked_list* ll) {
+    
+}
---
+++ linked_list/linked_list.h
@@ -0,0 +1,38 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <stdint.h>
+
+#include "inc/spinlock.h"
+
+typedef uint64_t pkey_t;
+typedef uint64_t pval_t;
+
+typedef struct entry {
+    pkey_t key;
+    pval_t value;
+}entry_t;
+
+struct ll_node {
+    entry_t e;
+    spinlock_t lock;
+    struct ll_node* next;
+}
+
+struct linked_list {
+    struct ll_node *head, *tail;
+}
+
+#define node_key(node) ((node)->e.k)
+#define node_val(node) ((node)->e.v)
+
+extern struct linked_list ll_init();
+extern int ll_insert(struct linked_list* ll, pkey_t key, pval_t val);
+extern int ll_update(struct linked_list* ll, pkey_t key, pval_t val);
+extern int ll_lookup(struct linked_list* ll, pkey_t key);
+extern int ll_remove(struct linked_list* ll, pkey_t key);
+extern int ll_range(struct linked_list* ll, pkey_t low, pkey_t high, entry_t* res_arr);
+extern void ll_print(struct linked_list* ll);
+extern void ll_destory(struct linked_list* ll);
+
+#endif
